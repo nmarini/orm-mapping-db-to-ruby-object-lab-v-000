d42768872453a41f5eda1379ef559841211e08db
Nicolas Marini <nmarini44@gmail.com>
2018-08-27 23:14:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,17 @@
 class Student
   attr_accessor :id, :name, :grade
 
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 10
+      LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql, x).map{|row| self.new_from_db(row)}
+
+  end
+
   def self.students_below_12th_grade
     sql = <<-SQL
       SELECT * FROM students
