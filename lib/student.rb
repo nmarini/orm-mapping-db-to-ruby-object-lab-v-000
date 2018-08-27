06c603bc94dde28151962f7dece668ddb10fe039
Nicolas Marini <nmarini44@gmail.com>
2018-08-27 23:17:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,8 +8,8 @@ class Student
       LIMIT 1
     SQL
 
-  t=  DB[:conn].execute(sql).map{|row| self.new_from_db(row)}
-binding.pry
+    DB[:conn].execute(sql).map{|row| self.new_from_db(row)}.first
+
   end
 
   def self.first_X_students_in_grade_10(x)
