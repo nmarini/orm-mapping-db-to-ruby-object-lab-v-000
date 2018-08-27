a0bfb1be096ab858d6ca83662da2a308b101a2aa
Nicolas Marini <nmarini44@gmail.com>
2018-08-27 23:13:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,16 @@
 class Student
   attr_accessor :id, :name, :grade
 
+  def self.all_students_below_12th
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade < 12
+    SQL
+
+    DB[:conn].execute(sql).map{|row| self.new_from_db(row)}
+
+  end
+
   def self.all_students_in_grade_9
     sql = <<-SQL
       SELECT * FROM students
